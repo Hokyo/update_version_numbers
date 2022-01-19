03bdaa6b660dacb99c405006f10e8dce962c546c
Robert Hahn <877263+Maschina@users.noreply.github.com>
2022-01-19 12:40:59 +0100

    Update update_version_numbers.sh

--- update_version_numbers.sh
+++ update_version_numbers.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# update_version_numbers.sh
+# @desc Auto-increment the build number every time code is committed.
+# @usage
+# 1. Select: your Target in Xcode
+# 2. Select: Build Phases Tab
+# 3. Select: Add Build Phase -> Add Run Script
+# 4. Paste code: `$SRCROOT/Scripts/update_version_numbers.sh`
+# 5. Drag the "Run Script" below "Link Binaries With Libraries"
+# 6. Insure that your starting build number is set to a whole integer and not a float (e.g. 1, not 1.0)
  
 # Current branch
 branch=$(git rev-parse --abbrev-ref HEAD)
@@ -6,7 +16,7 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 tag=$(git describe --tags $(git rev-list master --tags --max-count=1))
 
 # Check if in release branch within Git flow
-if [[ $branch =~ ^release\/([0-9]+)\.([0-9]+)(\.([0-9]+))?$ ]]; then 
+if [[ $branch =~ ^release\/([0-9]+)\.([0-9]+)(\.([0-9]+))? ]]; then 
   tagMajorNumber="${BASH_REMATCH[1]}"
   tagMinorNumber="${BASH_REMATCH[2]}"
   tagPatchNumber="${BASH_REMATCH[4]}"
@@ -14,23 +24,39 @@ if [[ $branch =~ ^release\/([0-9]+)\.([0-9]+)(\.([0-9]+))?$ ]]; then
   echo "You are currently on a release branch. Version: ${VersionString}"
 
 # Check if in hotfix branch within Git flow
-elif [[ $branch =~ ^hotfix\/([0-9]+)\.([0-9]+)(\.([0-9]+))?$ ]]; then
+elif [[ $branch =~ ^hotfix\/([0-9]+)\.([0-9]+)(\.([0-9]+))? ]]; then
   tagMajorNumber="${BASH_REMATCH[1]}"
   tagMinorNumber="${BASH_REMATCH[2]}"
   tagPatchNumber="${BASH_REMATCH[4]}"
   [[ -z "$tagPatchNumber" ]] && VersionString="${tagMajorNumber}.${tagMinorNumber}" || VersionString="${tagMajorNumber}.${tagMinorNumber}.${tagPatchNumber}"
   echo "You are currently on an hotfix branch. Version: ${VersionString}"
 
+# Check if in feature branch within Git flow
+elif [[ $branch =~ ^feature ]] && [[ $tag =~ ^([0-9]+)\.([0-9]+)(\.([0-9]+))? ]]; then
+  tagMajorNumber="${BASH_REMATCH[1]}"
+  tagMinorNumber="${BASH_REMATCH[2]}"
+  tagPatchNumber="${BASH_REMATCH[4]}"
+  [[ -z "$tagPatchNumber" ]] && VersionString="${tagMajorNumber}.${tagMinorNumber}" || VersionString="${tagMajorNumber}.${tagMinorNumber}.${tagPatchNumber}+"
+  echo "You are currently on an feature branch. Version: ${VersionString}"
+
 # We are most likely on developer branch. Use tag name as version.
-elif [[ $branch =~ ^develop$ ]] && [[ $tag =~ ^([0-9]+)\.([0-9]+)(\.([0-9]+))?$ ]]; then
+elif [[ $branch =~ ^develop$ ]] && [[ $tag =~ ^([0-9]+)\.([0-9]+)(\.([0-9]+))? ]]; then
+  tagMajorNumber="${BASH_REMATCH[1]}"
+  tagMinorNumber="${BASH_REMATCH[2]}"
+  tagPatchNumber="${BASH_REMATCH[4]}"
+  [[ -z "$tagPatchNumber" ]] && VersionString="${tagMajorNumber}.${tagMinorNumber}" || VersionString="${tagMajorNumber}.${tagMinorNumber}.${tagPatchNumber}+"
+  echo "You are on a developer branch. Version (from last tag name): ${VersionString}"
+
+# We are most likely on master branch. Use tag name as version.
+elif [[ $branch =~ ^master$ ]] && [[ $tag =~ ^([0-9]+)\.([0-9]+)(\.([0-9]+))? ]]; then
   tagMajorNumber="${BASH_REMATCH[1]}"
   tagMinorNumber="${BASH_REMATCH[2]}"
   tagPatchNumber="${BASH_REMATCH[4]}"
   [[ -z "$tagPatchNumber" ]] && VersionString="${tagMajorNumber}.${tagMinorNumber}" || VersionString="${tagMajorNumber}.${tagMinorNumber}.${tagPatchNumber}"
-  echo "You are on a developer branch. Version (from last tag name): ${VersionString}"
+  echo "You are on a master branch. Version (from last tag name): ${VersionString}"
 
 else
-  echo "Version cannot be extracted from git. Neither tag name nor branch provides the respective version information. Are you using Git-flow or the proper semantic versioning? Make also sure that you are on a developer branch, if not on release or hotfix branch." 1>&2
+  echo "Version cannot be extracted from git. Neither tag name (${tag}) nor branch (${branch}) provides the respective version information. Are you using Git-flow or the proper semantic versioning? Make also sure that you are on a developer branch, if not on release or hotfix branch." 1>&2
   exit 4
 fi
 
@@ -41,7 +67,8 @@ buildNumber=$(expr $(git rev-list develop --count) - $(git rev-list HEAD..develo
 
 # Update Info.plist
 echo "Updating marketing version number '${VersionString}' to ${PROJECT_DIR}/${INFOPLIST_FILE}."
-xcrun agvtool new-marketing-version "${VersionString}"
+#xcrun agvtool new-marketing-version "${VersionString}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $VersionString" "${PROJECT_DIR}/${INFOPLIST_FILE}"
 echo "Updating build number '${buildNumber}' to ${TARGET_BUILD_DIR}/${INFOPLIST_PATH}."
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
 if [ -f "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}.dSYM/Contents/Info.plist" ]; then
